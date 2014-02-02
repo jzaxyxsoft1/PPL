afe5db1a3a86ec7bfe5d828e8cf59fa026b14d9b
jzaxyxsoft1 <jzaxyxsoft@163.com>
2014-02-03 01:41:02 +0800

    Initial commit

--- controller/order.js
+++ controller/order.js
@@ -1,3 +1,22 @@
 /**
  *订单
  */
+exports.get=function (req,res){
+    var t=req.query['t'];
+    switch (t){
+        case 'n': //新建订单
+            break;
+        case 's': //订单状态
+            break;
+    }
+};
+exports.post=function(req,res){
+    var t= req.body['t'];
+    var obj= req.body['obj'];
+    switch (t){
+        case 'sn':
+            break;
+        case '':
+            break;
+    }
+};
