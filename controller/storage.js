dd21b4748a0115f627fc8b626f99dd739786c597
jzaxyxsoft1 <jzaxyxsoft@163.com>
2014-02-08 22:39:24 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	public/javascripts/KOExt.js

--- controller/storage.js
+++ controller/storage.js
@@ -3,6 +3,7 @@
  */
 var Svc = require('Svc').Svc;
 var helper = require('Svc').HttpHelper;
+var async = require('async');
 exports.get = function (req, res) {
     var t = req.query['t'];
     var m = req.query['m'];
@@ -27,7 +28,7 @@ exports.get = function (req, res) {
             res.render('storage/package.ejs');
             break;
         case 'stockin'://入库
-            res.render('storage/stockin.ejs', {u:req.currentUser});
+            res.render('storage/stockin.ejs', {u: req.currentUser});
             break;
         case 'stockinconfirm'://入库确认
             res.render('storage/stockinc.ejs');
@@ -37,20 +38,60 @@ exports.get = function (req, res) {
             break;
     }
 }
-exports.post=function (req,res){
-    var t= req.body['t'].toLowerCase();
-    switch (t){
+exports.post = function (req, res) {
+    var t = req.body['t'].toLowerCase();
+    switch (t) {
         case 'savepackages':
-            var objs=JSON.parse(req.body['objs']);
-            Svc.db.Package.insert(objs, function (e){
+            var objs = JSON.parse(req.body['objs']);
+            Svc.db.Package.insert(objs, function (e) {
                 res.json(true);
             })
             break;
         case 'stockinconfirm':
-            var obj=JSON.parse(req.body['obj']);
-            Svc.db.StockIn.update({_id:obj._id},{$set:{Status:obj.Status, Items:obj.Items}},function (e){
-                res.json({msg:e ==null,error:e});
-            });
+            var obj = JSON.parse(req.body['obj']);
+            async.waterfall({
+                /**
+                 * 单据更新
+                 */
+                bill: function (cb) {
+                    Svc.db.StockIn.update({_id: obj._id}, {$set: {Status: obj.Status, Items: obj.Items, StockInComfirmOperator: req.currentUser}}, cb);
+                },
+                /**
+                 * 库存更新
+                 */
+                update: function (cb) {
+                    async.each(obj.Items, function (i, icb) {
+                        Svc.db.Storage.findOne({'RelativeObj.Item1': i.RelativeObj.Item1, 'Stock.Value': i.Stock.Value},
+                            function (ee, d) {
+                                if (d) {
+                                    var am = d.Amount + i.Amount;
+                                    var cs = Math.round((d.Amount * d.UnitCost + i.Amount * i.UnitCost) / am, 2);
+                                    Svc.db.Storage.update({_id: d._id}, {$set: {Amount: am, Cost: cs}}, icb);
+                                }
+                                else {
+                                    Svc.db.Storage.insert({
+                                        _id: Svc.db.Storage.ObjectID().toString()(),
+                                        RelativeObj: i.RelativeObj,
+                                        Amount: i.Amount,
+                                        Model: i.Model,
+                                        Unit: i.Unit,
+                                        UnitCost: i.UnitCost,
+                                        Cost: Math.round((i.Amount * i.UnitCost), 2),
+                                        Stock: i.Stock
+                                    }, icb);
+                                }
+                            });
+                    }, cb);
+                },
+                /**
+                 * 删除零库存
+                 */
+                del: function (cb) {
+                    Svc.db.Storage.remove({Amount: 0}, cb);
+                }
+            }, function (e) {
+                res.json({msg: e == null, error: e});
+            })
             break;
     }
 }
