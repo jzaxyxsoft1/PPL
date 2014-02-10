04d9fe3eccdcdb289d59372de00020fc427101a0
jzaxyxsoft1 <jzaxyxsoft@163.com>
2014-02-10 17:23:24 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	public/javascripts/KOExt.js

--- controller/file.js
+++ controller/file.js
@@ -8,14 +8,11 @@ exports.post = function (req, res) {
     var _d = new Date();
     _d = (_d.getFullYear().toString() + (_d.getMonth() + 1) + _d.getDate());
     var p = req.body['p'];
-    var fPath = "/upload/" + p;
+    var fPath = "/upload/" + p + '/' + _d;
     if (!fs.existsSync(__dirname + '/../public' + fPath)) {
         fs.mkdirSync(__dirname + '/../public' + fPath);
     }
-    fPath = fPath + '/' + _d;
-    if (!fs.existsSync(__dirname + '/../public' + fPath)) {
-        fs.mkdirSync(__dirname + '/../public' + fPath);
-    }
+
     var filePath = req.files.file.path;
     var extName = path.extname(req.files.file.name);
     //  var fileUrl = "/upload/" + p+'/'+_d + '/' + path.basename(filePath) + extName;
